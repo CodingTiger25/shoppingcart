f7b0907c22757ed43d2b7813467b013c78434416
Tony <emperialace@gmail.com>
2026-01-18 02:33:45 -0500

    Added pages

---
+++ src/HomePage.jsx
@@ -0,0 +1,11 @@
+
+export function ShopPage() {
+  return (
+    <div>
+      <h1>Welcome to the Shop Page</h1>
+      <p>Here you can browse and purchase products.</p>
+    </div>
+  );
+}           
+
+
---
+++ src/Product.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function ShopPage() {
+  return (
+    <div>
+      <h1>Welcome to the Shop Page</h1>
+      <p>Here you can browse and purchase products.</p>
+    </div>
+  );
+}
+
+
+
+export default ShopPage;
